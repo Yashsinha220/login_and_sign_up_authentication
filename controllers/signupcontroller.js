bb9950f892d139c9c02afc1a049d3172cff3e9ba
Yash sinha <sinhayash63@gmail.com>
2022-12-11 16:16:43 +0530

    some improvement

--- controllers/signupcontroller.js
+++ controllers/signupcontroller.js
@@ -3,45 +3,47 @@ const { rawListeners } = require("../models/User");
 
 const userdetail = require("../models/User");
 const getsignupform = async (req, res) => {
-  res.status(200).sendFile(path.join(__dirname, "../public/signup.html"));
+  res.status(200).send(await userdetail.find({}))
 };
 
 const getloginform = async (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/login.html",))
-}
+  res.status(200).send("sending the login form");
+};
 
 const usersignup = async (req, res) => {
   const { uname, email, password } = req.body;
 
-  if(!uname || !email ||!password) {res.status(400).sendFile("invalid request")}
+  if (!uname || !email || !password) {
+    res.status(400).send("invalid request");
+  }
 
   const prevUser = await userdetail.findOne({ email });
   if (prevUser) {
     res.send("user already exist");
   } else {
-    await userdetail.create({  uname, email, password });
-    res.send("thanks for entering the value ");
+    await userdetail.create({ uname, email, password });
+    res.send("Account created ");
   }
   console.log(uname, email, password);
 };
 
-
 const userLogin = async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    res.send("please enter the details");
+  }
 
-    const {email , password } = req.body;
-    if(!email ||!password) {res.send("please enter the details")};
+  // need to find the one element hence we use the findone
 
-    // need to find the one element hence we use the findone
+  const users = await userdetail.findOne({ email, password });
 
-   const users= await userdetail.findOne({email , password });
+  // need to compare teh
 
-    // need to compare teh 
+  if (users && email == users.email && users && password == users.password) {
+    res.send("hi welcome to our site");
+  } else {
+    res.status(400).send("invalid credetials");
+  }
+};
 
-    if(email == users.email && password == users.password){
-    res.send("hi welcome to our site")
-    }
-    else{
-        res.send("invalid credetials")
-    }
-}
-module.exports = { userLogin, usersignup  , getsignupform , getloginform};
+module.exports = { userLogin, usersignup, getsignupform, getloginform };
